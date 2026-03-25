06aadda0ebb85311547e26de53c9c3e27bcbfc4e
eropRFSlee <li_ea@rfs.ru>
2026-03-25 17:23:07 +0300

    Update bx24.py

--- bx24.py
+++ bx24.py
@@ -5,10 +5,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-SMTP_SERVER = "smtp.mail.ru"
-SMTP_PORT = 465
-SENDER_EMAIL = "egorka_li@mail.ru"
-SENDER_PASSWORD = "JdBqwlzz0wH0tueZXWIY"
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+SENDER_EMAIL = "egor58241@gmail.com"
+SENDER_PASSWORD = "lrmg pate yxhj tkpe"
 RECIPIENT_EMAIL = "eroproralee@yandex.ru"
 SUBJECT = "Тестовое письмо"
 BODY = "Привет! Это тестовое письмо, отправленное через Python."
@@ -20,7 +20,8 @@ def send_email():
     msg["Subject"] = SUBJECT
     msg.attach(MIMEText(BODY, "plain", "utf-8"))
     
-    with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as server:
+    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+        server.starttls()
         server.login(SENDER_EMAIL, SENDER_PASSWORD)
         server.send_message(msg)
     
