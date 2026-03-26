120fcdddd60b1a1772291891fca83ad723e8f5d2
eropRFSlee <li_ea@rfs.ru>
2026-03-26 10:52:19 +0300

    Update bx24.py

--- bx24.py
+++ bx24.py
@@ -1,2 +1,35 @@
-import datetime
-print(f"✅ Код выполнился в {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from flask import Flask
+
+app = Flask(__name__)
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+SENDER_EMAIL = "egor58241@gmail.com"
+SENDER_PASSWORD = "lrmg pate yxhj tkpe"
+RECIPIENT_EMAIL = "eroproralee@yandex.ru"
+SUBJECT = "Тестовое письмо"
+BODY = "Привет! Это тестовое письмо, отправленное через Python."
+
+def send_email():
+    msg = MIMEMultipart()
+    msg["From"] = SENDER_EMAIL
+    msg["To"] = RECIPIENT_EMAIL
+    msg["Subject"] = SUBJECT
+    msg.attach(MIMEText(BODY, "plain", "utf-8"))
+    
+    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+        server.starttls()
+        server.login(SENDER_EMAIL, SENDER_PASSWORD)
+        server.send_message(msg)
+    
+    return "OK"
+
+@app.route('/')
+def index():
+    return send_email()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
